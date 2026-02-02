067d4d8a1cf8196ce0955afadc75f5788a4e9bfb
ommahale014-web <ommahale014@gmail.com>
2026-02-02 12:23:27 +0530

    step1 done Next.js app router setup with jsx and tailwind

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl text-red-500 font-extrabold">
+        Student Study Companion
+      </h1>
+    </main>
+  );
+}
